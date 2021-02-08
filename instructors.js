6267cf6aeae8e08e8bada6c4a54a1c3de6adb118
Jhonata <jhomcosta@gmail.com>
2021-02-07 22:43:34 -0300

    instructors list done

--- instructors.js
+++ instructors.js
@@ -3,6 +3,10 @@ const data = require("./data.json")
 const  { age, date } = require('./utils')
 
 
+
+exports.index = function(req, res) {
+    return res.render("instructors/index", { instructors: data.instructors})
+}
 // show
 exports.show = function (req, res) {
     const { id } = req.params
@@ -98,7 +102,8 @@ exports.put = function(req, res) {
     const instructor = {
         ...foundInstructor,
         ...req.body,
-        birth: Date.parse(req.body.birth)
+        birth: Date.parse(req.body.birth),
+        id: Number(req.body.id)
     }
 
     data.instructors[index] = instructor
